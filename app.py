7febbef43a636b9b7cc9f51b42cf5d32b7c71dc7
Junying (Alice) Fang <31668264+fjying@users.noreply.github.com>
2023-05-01 15:29:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = dash.Dash(external_stylesheets=[dbc.themes.CERULEAN])
 app.title = 'ORFEUS Data Website'
 
 # dropbox access token
-APP_KEY = "uya754jfrwnq1o8"
-APP_SECRET = "qcvxuobkaviuvc4"
-REFRESH_TOKEN = "d7NocTXgLgwAAAAAAAAAAeuKMBIVWwMJK3fCD1t9rtcwsrHXV1DMDwyQ_-SJhNXp"
+APP_KEY = "XX"
+APP_SECRET = "XX"
+REFRESH_TOKEN = "XXX"
 dbx = dropbox.Dropbox(app_key=APP_KEY,
                       app_secret=APP_SECRET,
                       oauth2_refresh_token=REFRESH_TOKEN)
